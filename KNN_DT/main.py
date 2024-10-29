a185d434b74be151d3721f5b07ce0b376b5beb3e
Jairik <jairikmc@gmail.com>
2024-10-29 01:48:59 -0400

    Cleaning code and cross-validation testing

--- KNN_DT/main.py
+++ KNN_DT/main.py
@@ -36,60 +36,43 @@ knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(x_train, y_train)
 y_pred = knn.predict(x_test)
 print("KNN Independent Test Accuracy: ", knn.score(x_test, y_test), "\nClassification Report:\n", classification_report(y_pred, y_test), "\n", confusion_matrix(y_pred, y_test))
-''' Utilizing scikit-learn's grid search to test different model parameters '''
-param_grid = [
-    {'n_neighbors':[3, 4, 6, 7]}, {'weights': ['uniform', 'distance']}
-]
-grid_search = GridSearchCV(esitmator=knn, param_grid=param_grid, cv=5, scoring='accuracy')
+''' Utilizing scikit-learn's grid search to test different KNN model parameters '''
+n_neighbors_counts = [i for i in range(1, 50)]
+param_grid = [{
+    'n_neighbors': n_neighbors_counts, 'weights': ['uniform', 'distance'], 'metric': ['euclidean', 'manhattan']
+}]
+grid_search = GridSearchCV(estimator=knn, param_grid=param_grid, cv=10, scoring='accuracy')
 grid_search.fit(x_train, y_train)
+print(f"\tBest Parameters: {grid_search.best_params_}")
+print(f"\tBest Cross-Validation Score: {grid_search.best_score_}")
+knn_final = grid_search.best_estimator_
+print(f"\tScore with testing data: {knn_final.score(x_test, y_test)}\n\
+    {classification_report(knn_final.predict(x_test), y_test)}")
 
 # Re-training and testing DT
 dt = DecisionTreeClassifier(criterion='entropy')
 dt.fit(x_train, y_train)
 y_pred = dt.predict(x_test)
 print("Decision Tree Independent Test: ", dt.score(x_test, y_test), "\nClassification Report:\n", classification_report(y_pred, y_test), "\n", confusion_matrix(y_pred, y_test))
+''' Utilizing scikit-learn's grid search to test different Decision Tree model parameters '''
+max_depth_counts = [i for i in range(1, 20)]
+max_depth_counts.append(None)  # No max depth
+min_sample_split_counts = [i for i in range(2, 10)]
+param_grid = [{
+    'criterion':['entropy', 'gini'], 
+     'max_depth': max_depth_counts, 
+     'min_samples_split': min_sample_split_counts
+}]
+grid_search = GridSearchCV(estimator=DecisionTreeClassifier(), param_grid=param_grid, cv=10, scoring='accuracy')
+grid_search.fit(x_train, y_train)
+print(f"\tBest Parameters: {grid_search.best_params_}")
+print(f"\tBest Cross-Validation Score: {grid_search.best_score_}")
+dt_final = grid_search.best_estimator_
+print(f"\tScore with testing data: {dt_final.score(x_test, y_test)}\n\
+    {classification_report(dt_final.predict(x_test), y_test)}")
 
-
-''' Optimizing Models '''
-# Testing KNN with different n_neighbors and weights using cross-validation
-# for i in range(1, 50):
-#     knn_test_n = KNeighborsClassifier(n_neighbors=i, weights = 'uniform', n_jobs=-1)
-#     knn_test_w = KNeighborsClassifier(n_neighbors=i, weights = 'distance', n_jobs=-1)
-#     knn_test_n.fit(x_train, y_train)
-#     knn_test_w.fit(x_train, y_train)
-#     print(f"KNN {i} uniform weights: ", (cross_val_score(knn_test_n, x_test, y_test)).mean()) # folds default to 5 
-#     print(f"KNN {i} distance weights: ", (cross_val_score(knn_test_w, x_test, y_test)).mean(), "\n") 
-# Best results were accomplished at k=3 and distance weights (.98333334)
-
-knn_final = KNeighborsClassifier(n_neighbors=3, weights='distance', n_jobs=-1)
-
-# Testing Decision Tree with various different parameters using cross-validation
-# dt_test_cg = DecisionTreeClassifier(criterion='gini')
-# dt_test_ce = DecisionTreeClassifier(criterion='entropy')
-# print(f"DT Gini default: {cross_val_score(dt_test_cg, x, y).mean()} \n \
-#     DT Entrophy Default: {cross_val_score(dt_test_ce, x, y)}\n")
-# for i in range(5, 30, 5):  # Testing max depth to prevent overly-complex trees
-#     dt_test_cg_md = DecisionTreeClassifier(criterion='gini', max_depth=i)
-#     dt_test_ce_md = DecisionTreeClassifier(criterion='entropy', max_depth=i)
-#     print(f"DT Gini Max Depth {i}: {cross_val_score(dt_test_cg_md, x, y).mean()} \n \
-#     DT Entrophy Max Depth {i}: {cross_val_score(dt_test_ce_md, x, y).mean()}\n")
-#     for j in range(3, 10): # Testing minimum sample split to potentially prevent overfitting
-#         dt_test_cg_ss = DecisionTreeClassifier(criterion='gini', max_depth=i, min_samples_split=j)
-#         dt_test_ce_ss = DecisionTreeClassifier(criterion='entropy', max_depth=i, min_samples_split=j)
-#         print(f"DT Gini Max Depth {i} Sample Split {j}: {cross_val_score(dt_test_cg_ss, x, y).mean()} \n \
-#         DT Entrophy Max Depth {i} Sample Split {j}: {cross_val_score(dt_test_ce_ss, x, y).mean()}\n")
-# Interestingly, best fit is around max depth 10 sample split 8-9 for Gini (around .97)
-
-dt_final = DecisionTreeClassifier(criterion='gini', max_depth=10, min_samples_split=8)
-
-print(f"KNN accuracy of {cross_val_score(knn_final, x, y).mean()} vs Decision Tree accuracy of {cross_val_score(dt_final, x, y).mean()}")  # KNN Wins!
-
-''' Showing independent test results of final knn '''
-knn_final.fit(x_train, y_train)
-y_pred = knn_final.predict(x_test)
-print(f"Final KNN Accuracy: {knn_final.score(x_test, y_test)}")
-print(f"Final KNN Classification Report:\n{classification_report(y_pred, y_test)}")
-print("Final KNN Confusion Matrix: ")
+''' Showing Confusion Matrix for Optimal Model '''
+# print("Final KNN Confusion Matrix: ")
 cm = ConfusionMatrixDisplay.from_estimator(knn_final, x_test, y_test)  
 plt.show()
 
@@ -102,7 +85,7 @@ for i in test_sizes:
     knn_final.fit(x_train, y_train)
     knn_models_scores.append((knn_final.score(x_test, y_test))*100)
 
-print("Final KNN Test Size Bar Figure: ")    
+# print("Final KNN Test Size Bar Figure: ")    
 plt.bar(x=test_sizes, height=knn_models_scores, color=['blue'], edgecolor='black')
 plt.ylim(94, 100)
 plt.title("KNN Test Size Accuracies")
