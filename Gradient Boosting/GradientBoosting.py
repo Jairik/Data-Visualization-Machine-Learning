2f9c6764ad73042a715b009d48e635506b39bb20
Jairik <jairikmc@gmail.com>
2024-11-01 02:20:03 -0400

    Began Gradient Boosting Classification Optimization
    Currently running some tests now to find-tune parameters. Also re-structured directory a bit in this commit to take out 15-puzzle game, as it will have it's own dedicated repo when combining with AI (A*)

---
+++ Gradient Boosting/GradientBoosting.py
@@ -0,0 +1,43 @@
+''' JJ McCauley '''
+import pandas as pd
+import numpy as np  # Potentially not needed
+import matplotlib as plt
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import xgboost as xgb
+
+# Collecting features and targets for testing and training sets
+ftdf = pd.read_csv('FoodTypeDataset.csv')
+x = ftdf.iloc[:, :-1].values
+y = ftdf.iloc[:, -1].values
+# All Features
+# print(x.shape)
+# print(y.shape)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.3, random_state=42)
+
+# Creating & self testing Gradient Boosting Classifier
+gbmodel = GradientBoostingClassifier()  # Default parameters for now
+gbmodel.fit(x, y)
+print("", gbmodel.score(x, y))
+
+# Creating test parameters
+learning_rate = [i for i in np.arange(0.0, 4.0, 0.1)]
+n_estimators = [j for j in range(50, 250, 50)]  # Will refine later
+subsample = [s for s in np.arange(0.1, .5, .1)]
+min_samples_split = [m for m in range(2, 15)]
+min_samples_leaf = [msl for msl in range(1, 15)]
+max_depth = [md for md in range(5, 50)]
+max_depth.append(None)
+params = [{
+    'learning_rate':learning_rate, 'n_estimators':n_estimators, 'subsample':subsample,
+    'min_samples_split':min_samples_split, 'min_samples_leaf':min_samples_leaf, 
+    'max_depth':max_depth, 'random_state':[42]
+}]
+
+# Creating grid gb model to test parameters
+grid = GridSearchCV(gbmodel, params, cv=10)
+grid.fit(x_train, y_train)
+print(f"Best Parameters: {grid.best_params_}")
+best_gbmodel = grid.best_estimator_
+y_pred = best_gbmodel.predict()
---
+++ Gradient Boosting/README.md
@@ -0,0 +1,7 @@
+# Optimizing Classification Dataset
+
+### Task:
+Given a dataset (~60 features and ~970 samples), try to optimize using any classification model.
+
+### Approach:
+Utilizing gradient boosting algorithm from Sklearn, due to its . Once a foundation is built, I will then transfer to other libraries known for these algorithms.
