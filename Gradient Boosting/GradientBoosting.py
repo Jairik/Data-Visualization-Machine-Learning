4ce8fccc473447727de72f9a7282543e3239b7c2
Jairik <jairikmc@gmail.com>
2024-11-05 18:58:28 -0500

    Created final model with Classification Report & Confusion Matrix
    Done did all that stuff

--- Gradient Boosting/GradientBoosting.py
+++ Gradient Boosting/GradientBoosting.py
@@ -1,19 +1,14 @@
 ''' JJ McCauley '''
 import pandas as pd
-import numpy as np  # Potentially not needed
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import xgboost as xgb
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 
 # Collecting features and targets for testing and training sets
 ftdf = pd.read_csv('FoodTypeDataset.csv')
 x = ftdf.iloc[:, :-1].values
 y = ftdf.iloc[:, -1].values
-# All Features
-# print(x.shape)
-# print(y.shape)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=42)
 
 # Creating & self testing Gradient Boosting Classifier
@@ -40,17 +35,36 @@ params = {
 }
 
 # Creating grid gb model to test parameters
-grid = GridSearchCV(gbmodel, params, cv=4, n_jobs=-1, verbose=2)
-grid.fit(x_train, y_train)
-print(f"Best Parameters: {grid.best_params_}")
-best_gbmodel = grid.best_estimator_
-y_pred = best_gbmodel.predict(x_test)
-print("Score: ", best_gbmodel.score(x_test, y_test))
+# grid = GridSearchCV(gbmodel, params, cv=4, n_jobs=-1, verbose=2)
+# grid.fit(x_train, y_train)
+# print(f"Best Parameters: {grid.best_params_}")
+# best_gbmodel = grid.best_estimator_
+# y_pred = best_gbmodel.predict(x_test)
+# print("Score: ", best_gbmodel.score(x_test, y_test))
 
-# Best Parameters: {'learning_rate': 0.1, 'max_depth': 37, 'min_samples_leaf': 3, 'min_samples_split': 2, 
-# 'n_estimators': 200, 'random_state': 42, 'subsample': 0.4}
-# Score:  0.45714285714285713 (very low??)
+optimal_gb = GradientBoostingClassifier(learning_rate=.1, max_depth=38, max_features=13, min_samples_leaf=3,
+                                        min_samples_split=2, n_estimators=200, subsample=1.0, random_state=42)
+optimal_gb.fit(x_train, y_train)
+y_pred = optimal_gb.predict(x_test)
 
-# NEW BEST PARAMETERS:Best Parameters: {'learning_rate': 0.1, 'max_depth': 38, 'max_features': 13, 'min_samples_leaf': 3, 
-# 'min_samples_split': 2, 'n_estimators': 200, 'random_state': 42, 'subsample': 1.0}
-# Score:  0.5508021390374331
+# Printing Classification Report
+print(classification_report(y_test, y_pred))  
+
+# Showing heatmap of classification confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+fancycm = ConfusionMatrixDisplay(confusion_matrix=cm)
+fancycm.plot(cmap='Blues')
+plt.title("Confusion Matrix for Food Type Classification Model")
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.show()
+
+''' -- TESTING RESULTS --- 
+Old Best Parameters: {'learning_rate': 0.1, 'max_depth': 37, 'min_samples_leaf': 3, 'min_samples_split': 2, 
+'n_estimators': 200, 'random_state': 42, 'subsample': 0.4}
+Score:  0.45714285714285713 (very low??)
+
+NEW BEST PARAMETERS: {'learning_rate': 0.1, 'max_depth': 38, 'max_features': 13, 'min_samples_leaf': 3, 
+'min_samples_split': 2, 'n_estimators': 200, 'random_state': 42, 'subsample': 1.0}
+Score:  0.5508021390374331
+'''
